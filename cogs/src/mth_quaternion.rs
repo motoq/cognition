813e2ae24b1f4209a68d542b05d40f82e68a6a12
Kurt Motekew <motekew@verizon.net>
2026-09-09 00:20:25 -0400

    adding DCM to q

--- cogs/src/mth_quaternion.rs
+++ cogs/src/mth_quaternion.rs
@@ -7,12 +7,22 @@
  */
 
 
-//! Quaternion object meant to demonstrate conventions vs. be used
-//! as a quaternion library for heavy lifting.  See the unit test
-//! examples.  Emphasis is on conveying the need to indicate if
-//! the eigenaxis angle is a basis rotation (passive) or vector
-//! rotation (active) through the use of fundamental quaternion
-//! definitions and operations.
+//! Quaternion type not locked into a convention, suitable for aerospace
+//! attitude representation or computer graphics.  Quaternion multiplication
+//! is defined as the sum of elements resulting from the outer product of
+//! two quaternion 4D arrays.
+//!
+//! Either active or passive quaternions can be defined.  Operator
+//! overloading such that `q*v = q*p*q'` and `v*q = q'*p*q` (where `p`
+//! is a pure quaternion with imaginary part equal to `v`, and `q'` is
+//! the quaternion complex conjugate) allows unambiguous vector rotation
+//! vs. reference frame transformations.
+//!
+//! Given the (unit length) Euler axis `ehat` and rotation angle `theta`
+//! formulation of a unit quaternion `q = cos(theta/2) + ehat*sin(theta/2)`,
+//! `theta` is *active* if it rotates a vector within a reference frame.
+//! In contranst, `theta` is passive if instead the reference frame basis
+//! vectors are rotated the vector remains fixed in in space.
 //!
 //! # Author
 //!
@@ -22,8 +32,14 @@ use std::ops::Mul;
 
 use nalgebra as na;
 
-//use crate::utl_const::DEG_PER_RAD;
-//use crate::phy_const::DU_PER_ER;
+/// Square of quaternion norm magnitude to trigger normalization
+const TOL: f64 = 100.0*f64::EPSILON;
+
+/// DCM to quaternion alg selection factor.  0.25 requires
+/// the first solved for quaternion element to be at least
+/// 1/4 in magnitude or greater.  1.0 would result in the
+/// first needing to be 1/2 or larger (see notes in code).
+const KAPPA: f64 = 0.25;
 
 /// The quaternion is represented as real and imaginary components.
 /// There is no ambiguity w.r.t. which component is the scalar element
@@ -39,7 +55,7 @@ pub struct Quaternion {
 impl Default for Quaternion {
     /// # Return
     ///
-    /// * Identity quaternion
+    /// * Identity (zero rotation) unit quaternion
     ///
     fn default() -> Self {
         Self {
@@ -77,12 +93,13 @@ impl Quaternion {
     /// * Initialized quaternion of unit magnitude
     ///
     pub fn new_normalized(qr: f64, qi: &na::Vector3::<f64>) -> Self {
-        let mag = qr.abs() + qi.norm();
+        let mag = (qr*qr + qi.norm_squared()).sqrt();
         Self { qr: qr/mag, qi: qi/mag }
     }
 
     /// Initialize a unit quaternion given an angle and unit length rotation
-    /// axis.
+    /// axis.  Use case determines if angle is passive or active.  User must
+    /// ensure the Euler axis is unit length.
     ///
     /// # Arguments
     ///
@@ -96,9 +113,84 @@ impl Quaternion {
     pub fn from_angle_axis(angle: f64, axis:  &na::Vector3::<f64>) -> Self {
         Self { qr: (0.5*angle).cos(), qi: (0.5*angle).sin()*axis }
     }
+
+    /// Sets this quaternion based on the input direction cosine matrix.
+    /// The angle is passive and `q*vq` acts as a reference frame
+    /// transformation.
+    ///
+    /// Method based on discussion in "Quaternion Computation from a Geometric
+    /// Point of View" by Malcolm Shuster and Gregory Natanson.
+    ///
+    /// The first element computed that is greater than KAPPA in magnitude
+    /// is used as the starting point to determine the other elements.  See
+    /// "DCM to Quaternion and Back Again" by Kurt Motekew for details
+    ///
+    /// # Argument
+    ///
+    /// * dmc  Direction cosine matrix, reference frame transformation
+    ///        (passive).  No check is performed to ensure the input
+    ///        is orthonormal.
+    ///
+    /// # Return
+    ///
+    /// * Result  Ok if a unit quaternion can be extracted from this DCM.
+    ///
+    pub fn try_from_dcm(dcm: &na::Matrix3::<f64>) -> Result<Self, String> {
+        let mut tmp = 1.0 + dcm[(0,0)] + dcm[(1,1)] + dcm[(2,2)];
+        if tmp > KAPPA {
+            tmp = tmp.sqrt();
+            let d4 = 0.5/tmp;
+            return Ok(Self::new_normalized(
+                0.5*tmp,
+                &na::matrix![(dcm[(1,2)] - dcm[(2,1)])*d4 ;
+                             (dcm[(2,0)] - dcm[(0,2)])*d4 ;
+                             (dcm[(0,1)] - dcm[(1,0)])*d4],
+            ));
+        }
+
+        tmp = 1.0 + dcm[(0,0)] - dcm[(1,1)] - dcm[(2,2)];
+        if tmp > KAPPA {
+            tmp = tmp.sqrt();
+            let d4 = 0.5/tmp;
+            return Ok(Self::new_normalized(
+                (dcm[(1,2)] - dcm[(2,1)])*d4,
+                &na::matrix![0.5*tmp                      ;
+                             (dcm[(0,1)] + dcm[(1,0)])*d4 ;
+                             (dcm[(0,2)] + dcm[(2,0)])*d4],
+
+            ));
+        }
+
+        tmp = 1.0 - dcm[(0,0)] + dcm[(1,1)] - dcm[(2,2)];
+        if tmp > KAPPA {
+            tmp = tmp.sqrt();
+            let d4 = 0.5/tmp;
+            return Ok(Self::new_normalized(
+                (dcm[(2,0)] - dcm[(0,2)])*d4,
+                &na::matrix![(dcm[(0,1)] + dcm[(1,0)])*d4 ;
+                             0.5*tmp                      ;
+                            (dcm[(1,2)] + dcm[(2,1)])*d4],
+            ));
+        }
+
+        tmp = 1.0 - dcm[(0,0)] - dcm[(1,1)] + dcm[(2,2)];
+        if tmp > KAPPA {
+            tmp = tmp.sqrt();
+            let d4 = 0.5/tmp;
+            return Ok(Self::new_normalized(
+                (dcm[(0,1)] - dcm[(1,0)])*d4,
+                &na::matrix![(dcm[(0,2)] + dcm[(2,0)])*d4 ;
+                             (dcm[(1,2)] + dcm[(2,1)])*d4 ;
+                             0.5*tmp],
+            ));
+        }
+
+        return Err("Can't extract a Quaternion from input DCM".to_string()
+            + &dcm.to_string());
+    }
 }
 
-///! Public immutable accessor methods
+/// Public immutable accessor methods
 impl Quaternion {
     /// # Return
     ///
@@ -117,18 +209,31 @@ impl Quaternion {
     }
 }
 
-///! Mutable manipulation
+/// Mutable manipulation
 impl Quaternion {
-    /// Convert this quaternion to a unit quaternion
+    /// Normalize this quaternion (convert to unit length) if magnitude
+    /// differs from 1 by more than 100*f64::EPSILON.
+    ///
+    /// using criteria and method (Pade approximant) as described
+    /// by David Hammen on stackoverflow, Oct 17 '12
     pub fn normalize(&mut self) {
-        let mag = self.qr.abs() + self.qi.norm();
+        let n2 = self.qr*self.qr + self.qi.norm_squared();
+        let delta = (1.0 - n2).abs();
 
-        self.qr /= mag;
-        self.qi /= mag;
+        if delta < TOL {
+        } else if delta < 2.107342e-08 {
+            let norm_inv = 2.0/(1.0 + n2);
+            self.qr *= norm_inv;
+            self.qi *= norm_inv;
+        } else {
+            let norm_inv = 1.0/n2.sqrt();
+            self.qr *= norm_inv;
+            self.qi *= norm_inv;
+        }
     }
 }
 
-///! Immutable functions
+/// Immutable functions
 impl Quaternion {
     /// # Return
     ///
@@ -142,10 +247,26 @@ impl Quaternion {
     }
 }
 
-///! Immutable operator overload
+/// Immutable operator overload
 impl Mul for Quaternion {
     type Output = Self;
 
+    /// Quaternion multiplication via the outer product followed by
+    /// summation of all elements, grouped by real and complex types.
+    /// For passive quaternions, successive rotations are left to right
+    /// when combined with the v*q (q.conjugate()*v*q) operator for
+    /// reference frame transformations.  For active quaternions, sucessive
+    /// rotations are right to left (matrix algebra)  for reference frame
+    /// transformations.
+    ///
+    /// # Argument
+    ///
+    /// * rhs  The quaternion to multiply this one by.
+    ///
+    /// # Return
+    ///
+    /// * The product of this Quaternion and rhs, { self*rhs }
+    ///
     fn mul(self, rhs: Self) -> Self::Output {
         Quaternion {
             qr: self.qr*rhs.qr - self.qi[0]*rhs.qi[0]
@@ -165,22 +286,50 @@ impl Mul for Quaternion {
     }
 }
 
-/// fgiesen link and other useful references:
-/// <https://fgiesen.wordpress.com/
-///     2019/02/09/rotating-a-single-vector-using-a-quaternion/>
-/// <https://www.johndcook.com/blog/2021/06/16/faster-quaternion-rotations/>
-/// <https://blog.molecular-matters.com/
-///     2013/05/24/a-faster-quaternion-vector-multiplication/>
+/// fgiesen link and other useful references for the vector rotation operator:
+/// `https://fgiesen.wordpress.com/
+///     2019/02/09/rotating-a-single-vector-using-a-quaternion/`
+/// `https://www.johndcook.com/blog/2021/06/16/faster-quaternion-rotations/`
+/// `https://blog.molecular-matters.com/
+///     2013/05/24/a-faster-quaternion-vector-multiplication/`
 ///
 impl Mul<na::Vector3<f64>> for Quaternion {
     type Output = na::Vector3<f64>;
 
+    /// Overload Quaternion*Vector3 for the q*v*q.conjugate() operation where
+    /// v is treated as the imaginary part of a pure quaternion.
+    ///
+    /// # Argument
+    ///
+    /// * rhs  Vector to rotate or transform depending on if the angle
+    ///        associated with this quaternion is passive or active.
+    ///
+    /// # Return
+    ///
+    /// * q*v*q.conjugate(), a vector rotation if the angle defining the
+    ///   quaternion is passive.  If the angle is active, then this operation
+    ///   is a reference frame transformation.
+    ///
     fn mul(self, rhs: na::Vector3<f64>) -> Self::Output {
         let tt = 2.0*self.qi.cross(&rhs);
         rhs + self.qr*tt + self.qi.cross(&tt)
     }
 }
 
+    /// Overload Vector3*Quaternion for the q.conjugate()*v*q operation where
+    /// v is treated as the imaginary part of a pure quaternion.
+    ///
+    /// # Argument
+    ///
+    /// * rhs  Vector to transform or rotate depending on if the angle
+    ///        associated with this quaternion is passive or active.
+    ///
+    /// # Return
+    ///
+    /// * q.conjugate()*v*q, a vector reference frame transformation (basis
+    ///   vector rotation) if the angle defining the quaternion is passive.
+    ///   If the angle is active, then this is a vector rotation.
+    ///
 impl Mul<Quaternion> for na::Vector3<f64> {
     type Output = na::Vector3<f64>;
 
@@ -192,7 +341,7 @@ impl Mul<Quaternion> for na::Vector3<f64> {
 }
 
 
-///! IO
+/// IO
 impl std::fmt::Display for Quaternion {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
@@ -237,21 +386,31 @@ mod tests {
         let pos = na::Vector3::new(1.0, 1.0, 1.0);
         let qpos = Quaternion::new(0.0, &pos);
 
-        let pos_qx = qatt.conjugate()*qpos*qatt;
-
         let r1 = rotz(yaw);
         let r2 = roty(pitch);
         let r3 = rotx(roll);
-
         let ratt = r3*r2*r1;
-        let pos_rx = ratt*pos;
 
-        assert!((pos_rx - pos_qx.imaginary()).norm() < 10.0*f64::EPSILON);
-
-        let dv = (qatt*qpos*qatt.conjugate()).imaginary() - qatt*pos;
+        // DCM xform vs explicit q xform
+        let dv = ratt*pos - (qatt.conjugate()*qpos*qatt).imaginary();
         assert!(dv.norm() < 10.0*f64::EPSILON);
 
+        // DCM xform vs overload q xform
+        let dv = ratt*pos - pos*qatt;
+        assert!(dv.norm() < 10.0*f64::EPSILON);
+
+        // DCM xform vs. DCM derived q with overload
+        let qatt_dcm = Quaternion::try_from_dcm(&ratt).expect("Bad DCM");
+        let dv = ratt*pos - pos*qatt_dcm;
+        println!("q: {} {}", qatt, qatt_dcm);
+        assert!(dv.norm() < 10.0*f64::EPSILON);
+
+        // explicit xform vs. overload
         let dv = (qatt.conjugate()*qpos*qatt).imaginary() - pos*qatt;
+        assert!(dv.norm() < 10.0*f64::EPSILON);
+
+        // explicit q rotation vs. overload
+        let dv = (qatt*qpos*qatt.conjugate()).imaginary() - qatt*pos;
         assert!(dv.norm() < 10.0*f64::EPSILON);
     }
 }
